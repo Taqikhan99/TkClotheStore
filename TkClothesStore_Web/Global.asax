9897b076bd99d2d384e35c42067890189855da15
Taqi Pc <Taqi Pc@DESKTOP-LFRB8C6>
2022-11-12 21:20:37 +0500

    Add project files.

---
+++ TkClothesStore_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TkClothesStore_Web.MvcApplication" Language="C#" %>
---
+++ TkClothesStore_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TkClothesStore_Web.Startup))]
+namespace TkClothesStore_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
